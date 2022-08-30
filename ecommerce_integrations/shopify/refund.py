f896605f5ce86ef24481b7720d65b4b1ee473061
Sagan Rayner <sagan.rayner@strivebusiness.ca>
2022-08-30 14:57:42 +0000

    Added debit note, fixed taxes and item qty

--- ecommerce_integrations/shopify/refund.py
+++ ecommerce_integrations/shopify/refund.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 import frappe
 from erpnext.accounts.doctype.sales_invoice.sales_invoice import make_sales_return
 from frappe.utils import cint, cstr, getdate, nowdate
-from typing import List
+from typing import DefaultDict, List
 
 from ecommerce_integrations.shopify.constants import (
 	ORDER_ID_FIELD,
@@ -13,6 +13,7 @@ from ecommerce_integrations.shopify.constants import (
 )
 from ecommerce_integrations.shopify.utils import create_shopify_log
 from ecommerce_integrations.shopify.product import get_item_code
+from ecommerce_integrations.shopify.invoice import make_payament_entry_against_sales_invoice
 
 
 def prepare_credit_note(payload, request_id=None):
@@ -32,17 +33,61 @@ def prepare_credit_note(payload, request_id=None):
 		create_shopify_log(status="Error", exception=e, rollback=True)
 
 def make_credit_note(refund, setting, sales_invoice):
-	credit_note = create_credit_note(sales_invoice.name, setting)
+	if len(refund.get("refund_line_items")) > 0:
+		credit_note = create_credit_note(sales_invoice.name, setting)
 
-	if not refund["restock"]:
-		credit_note.update_stock = 0
+		if not refund["restock"]:
+			credit_note.update_stock = 0
 
-	return_items = [get_item_code(line.get("line_item")) for line in refund.get("refund_line_items")]
+		#return_items = [get_item_code(line.get("line_item")) for line in refund.get("refund_line_items")]
+		return_items = defaultdict()
+		for line in refund.get("refund_line_items"):
+			return_items[get_item_code(line.get("line_item"))] = {"qty": line.get("quantity"), "price": line.get("subtotal")}
 
-	_handle_partial_returns(credit_note, return_items)
 
-	credit_note.insert(ignore_mandatory=True)
-	credit_note.submit()
+		_handle_partial_returns(credit_note, return_items, sales_invoice)
+
+		credit_note.insert(ignore_mandatory=True)
+		credit_note.submit()
+		
+	
+	if len(refund.get("order_adjustments")) > 0:
+		amount = sum(float(adjustment.get("amount")) for adjustment in refund.get("order_adjustments"))
+		debit_note = create_debit_note(sales_invoice, amount, setting)
+		debit_note.insert(ignore_mandatory=True)
+		debit_note.submit()
+	make_payament_entry_against_sales_invoice(sales_invoice, setting)
+
+def create_debit_note(sales_invoice, amount, setting):
+	debit_note = create_credit_note(sales_invoice.name, setting)
+	debit_note.is_debit_note = 1
+
+	original_amount = debit_note.total + debit_note.total_taxes_and_charges
+
+
+	for item in debit_note.items:
+		item_percent = item.amount / original_amount
+		item.rate =  -item_percent * amount
+		item.qty = 0
+
+	for tax in debit_note.taxes:
+		# reduce total value
+		item_wise_tax_detail = json.loads(tax.item_wise_tax_detail)
+		new_tax_amt = 0.0
+
+		for item_code, tax_distribution in item_wise_tax_detail.items():
+			# item_code: [rate, amount]
+			if not tax_distribution[1]:
+				# Ignore 0 values
+				continue
+			return_percent = amount / original_amount
+			tax_distribution[1] *= return_percent
+			new_tax_amt += tax_distribution[1]
+
+		tax.tax_amount = new_tax_amt
+		tax.item_wise_tax_detail = json.dumps(item_wise_tax_detail)
+	return debit_note
+
 
 
 def create_credit_note(invoice_name, setting):
@@ -65,17 +110,21 @@ def get_sales_invoice(order_id):
 	if sales_invoice:
 		return frappe.get_doc("Sales Invoice", sales_invoice)
 
-def _handle_partial_returns(credit_note, returned_items: List[str]) -> None:
+def _handle_partial_returns(credit_note, returned_items: List[str], sales_invoice) -> None:
 	""" Remove non-returned item from credit note and update taxes """
 
 	item_code_to_qty_map = defaultdict(float)
-	for item in credit_note.items:
-		item_code_to_qty_map[item.item_code] += item.qty
+	for item in sales_invoice.items:
+		item_code_to_qty_map[item.item_code] -= item.qty
 
 	# remove non-returned items
 	credit_note.items = [
-		item for item in credit_note.items if item.sales_invoice_item in returned_items
+		item for item in credit_note.items if item.item_code in returned_items
 	]
+	for item in credit_note.items:
+		item.qty = -1 * returned_items[item.item_code]["qty"]
+		item.amount = -1 * returned_items[item.item_code]["price"]
+		item.rate = returned_items[item.item_code]["price"] / returned_items[item.item_code]["qty"]
 
 	returned_qty_map = defaultdict(float)
 	for item in credit_note.items:
